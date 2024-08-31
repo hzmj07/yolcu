3ccf155bacecdbde335f14710c4b2cc9916918be
Hazem ALJASEM <143966448+hzmj07@users.noreply.github.com>
2024-08-31 23:29:04 +0300

    Update firebaseConfig.js api key

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -14,15 +14,7 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC61rOqRny9DatxI30ZomDqFZAZkObJM-0",
-  authDomain: "testproj-76e6a.firebaseapp.com",
-  projectId: "testproj-76e6a",
-  storageBucket: "testproj-76e6a.appspot.com",
-  messagingSenderId: "207397823966",
-  appId: "1:207397823966:web:9f0010219773f5e0b5a94f",
-  measurementId: "G-YPNL7CV8J7"
-};
+{{ secrets.API_KEY }}
 
 console.log("kontrol")
 
